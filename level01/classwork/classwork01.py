3fd1e5a3644707db6444e628f880261de79e1599
Idumleli <levanikobaidze0@gmail.com>
2026-02-03 19:06:21 +0400

    second commit

---
+++ level01/classwork/classwork01.py
@@ -0,0 +1,13 @@
+# საკლასო სამუშაო
+
+name = "Levan"  # this is string
+print(name)
+
+surname = "Kobaidze"  # this is sting too
+print(surname)
+
+age = 28 # integer
+print(age)
+
+height = 1.76 # float
+print(height)
