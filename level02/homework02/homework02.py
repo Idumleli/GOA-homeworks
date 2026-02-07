8d9c8e19a3f4b6ace6d61f389e815f5a40726f16
Idumleli <levanikobaidze0@gmail.com>
2026-02-07 22:29:30 +0400

    third commit

---
+++ level02/homework02/homework02.py
@@ -0,0 +1,50 @@
+# Homework level 02
+
+# 1) ბრჭყალებში ჩაწერილ რიცხვსა "3" და ინტეჯერს შორის 3 განსხვავება მდგომარეობს შემდეგში, ბრჭყალებში ჩასმული რიცხვი აღიქმება პროგრამის მიერ როგორც string, რაც გულისხმობს იმას რომ იტეჯერსა და ბრჭყალებში ჩასმულ რიცხვს შორის მათემატიკური მოქმედება შეუძლებელია მაგ.: primt("3"+3) ეს მოქმედება არ შესრულდება როგორც რიცხვების შეკრება.
+
+# 2) 
+n1 = 3
+n2 = 7
+Addition = n1 + n2
+Substraction = n1 - n2
+Multiplication = n1 * n2
+Division = n1 / n2
+
+print(Addition)
+print(Substraction)
+print(Multiplication)
+print(Division)
+
+# 3)
+a = 57
+b = 4
+print(a + b) 
+print(a - b) 
+print(a * b) 
+print(a / b) 
+print(a // b) 
+print(a % b) 
+print(a ** b) 
+
+# 4)
+
+x = 10
+x = x + 5
+x = x * 2
+
+print(x)
+# შედეგში მივიღეთ 30; ეს იმიტომ რომ თავიდან x-ის მნიშვნელობა იყო 10, შემდეგ x-ის მნიშვნელობა შევცვალეთ და გამოვიდა რომ x=10+5 ანუ x=15, ამის შემდეგ x-ის მნიშვნელობა კვლავ შევცვვალეთ x = 15 * 2 და საბოლოოდ გამოგვივიდა x=30
+
+
+
+# 5)
+
+balance = 100
+balance = balance - 30
+balance = balance + 50
+balance = balance * 2
+
+print(balance)
+
+# 6)
+print ("Group 96 is the best" * 96)
